e63f84a0d1a9d82023d139dcfc1974177c7ba950
weakryan <99753350+weakryan@users.noreply.github.com>
2022-04-12 17:00:49 -0300

    Add files via upload

--- Ex3.java
+++ Ex3.java
@@ -5,24 +5,24 @@ import java.util.Scanner;
 public class Ex3 {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
-        int valor;
-        Boolean pausar = false;
-        char letra;
+        System.out.println("Insira um valor: ");
+        int valor = entrada.nextInt();
 
-        do{
-                System.out.print("Digite um valor: ");
-                valor = entrada.nextInt();
+        if (valor%2 == 0) {
+            System.out.println("O valor" + valor + " é par");
+        
 
-                 if(valor%2==0){
-                        System.out.println("O numero é par.");
-                }else{
-                     System.out.println("O numero é ímpar");
-                }
-       
-         
-        }
+        }else{
+            System.out.println("O valor " + valor + " é impar");
+
+
+            System.out.println("Explicação: Fiz o módulo de 2, se o resto era 0 é par, caso contrário é impar");
+
+        
     }
-       
+    }
+     
+}
 
         
     
