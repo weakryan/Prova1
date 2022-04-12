8b699214bce02054721f96cce982924f86c1e72f
weakryan <99753350+weakryan@users.noreply.github.com>
2022-04-12 16:59:11 -0300

    Add files via upload

---
+++ Ex3.java
@@ -0,0 +1,31 @@
+package Atividades.Prova1;
+import java.util.Scanner;
+
+
+public class Ex3 {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        int valor;
+        Boolean pausar = false;
+        char letra;
+
+        do{
+                System.out.print("Digite um valor: ");
+                valor = entrada.nextInt();
+
+                 if(valor%2==0){
+                        System.out.println("O numero é par.");
+                }else{
+                     System.out.println("O numero é ímpar");
+                }
+       
+         
+        }
+    }
+       
+
+        
+    
+    
+    
+
